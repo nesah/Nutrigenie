5d2b08ed55f4a2208cfb3f14d77ba599406adbfd
RED <rhabidpmendoza@gmail.com>
2024-07-11 00:32:54 +0800

    add progress bar

--- app/src/main/java/com/example/ai/MainActivity.java
+++ app/src/main/java/com/example/ai/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.ai;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progBar = findViewById(R.id.progBar);
+        progBar.setVisibility(ProgressBar.VISIBLE);
 
         new Handler().postDelayed(new Runnable() {
             @Override
