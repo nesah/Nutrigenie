2d54d2da094add00df66bcc727d6feeaa008c5d5
RED <rhabidpmendoza@gmail.com>
2024-07-12 11:39:18 +0800

    to add GA

--- app/src/main/java/com/example/ai/HomeActivity.java
+++ app/src/main/java/com/example/ai/HomeActivity.java
@@ -2,35 +2,21 @@ package com.example.ai;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.JsonReader;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    LinearLayout breakfastSection;
-    LinearLayout lunchSection;
-    LinearLayout dinnerSection;
-    LinearLayout snacksSection;
-
-    LinearLayout breakfastContent;
-    LinearLayout lunchContent;
-    LinearLayout dinnerContent;
-    LinearLayout snacksContent;
-
-    Button btn_breakfast;
-    Button btn_lunch;
-    Button btn_dinner;
-    Button btn_snacks;
-
+    LinearLayout breakfastSection, lunchSection, dinnerSection, snacksSection;
+    LinearLayout breakfastContent, lunchContent, dinnerContent, snacksContent;
     EditText input_calorie;
-
+    Button btn_breakfast, btn_lunch, btn_dinner, btn_snacks;
     Button btn_optimize;
 
     ArrayList<String> breakfastInputs = new ArrayList<>();
@@ -54,41 +40,14 @@ public class HomeActivity extends AppCompatActivity {
         dinnerContent = findViewById(R.id.dinnerContent);
         snacksContent = findViewById(R.id.snacksContent);
 
+        input_calorie = findViewById(R.id.input_calorie);
+
         btn_breakfast = findViewById(R.id.btn_breakfast);
         btn_lunch = findViewById(R.id.btn_lunch);
         btn_dinner = findViewById(R.id.btn_dinner);
         btn_snacks = findViewById(R.id.btn_snacks);
 
-        input_calorie = findViewById(R.id.input_calorie);
         btn_optimize = findViewById(R.id.btn_optimize);
-
-        btn_optimize.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                collectInput();
-
-                ReaderJson reader = new ReaderJson(v.getContext());
-                reader.readJsonFile("foods.json");
-
-                List<ReaderJson.FoodItem> foodItems = reader.getFoodItems();
-
-                String[] keywords = {"egg", "bread"}; // Example array of keywords
-
-                // Iterate through each keyword
-                for (String keyword : keywords) {
-                    List<ReaderJson.FoodItem> filteredItems = searchFoodItems(foodItems, keyword);
-
-                    // Find item with the lowest calories in filteredItems
-                    ReaderJson.FoodItem lowestCalorieItem = findItemWithLowestCalories(filteredItems);
-
-                    // Log or use the lowest calorie item as needed
-                    if (lowestCalorieItem != null) {
-                        Log.d("Filtered FoodItem", "Keyword: " + keyword + ", Lowest Calorie Item: " + lowestCalorieItem.toString());
-                        // Perform further operations with lowestCalorieItem
-                    }
-                }
-            }
-        });
 
         // Visibility of sections
         breakfastSection.setOnClickListener(new View.OnClickListener() {
@@ -141,9 +100,88 @@ public class HomeActivity extends AppCompatActivity {
                 addNewRow(snacksContent);
             }
         });
+
+        btn_optimize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get all user input
+                collectInput();
+
+                // read json file
+                ReaderJson reader = new ReaderJson(v.getContext());
+                reader.readJsonFile("foods.json");
+
+                // store json file contents
+                List<ReaderJson.FoodItem> foodItems = reader.getFoodItems();
+
+                // storage for data of input from json
+                ArrayList<String> bOutput = new ArrayList<>();
+                ArrayList<String> lOutput = new ArrayList<>();
+                ArrayList<String> dOutput = new ArrayList<>();
+                ArrayList<String> sOutput = new ArrayList<>();
+
+                // get food data in json
+                for (String keyword : breakfastInputs) {
+                    List<ReaderJson.FoodItem> filteredItems = searchFoodItems(foodItems, keyword);
+                    ReaderJson.FoodItem lowestCalorieItem = findItemWithLowestCalories(filteredItems);
+                    if (lowestCalorieItem != null) {
+                        String formattedItem = formatFoodItem(lowestCalorieItem);
+                        bOutput.add(formattedItem);
+                    }
+                }
+                for (String keyword : lunchInputs) {
+                    List<ReaderJson.FoodItem> filteredItems = searchFoodItems(foodItems, keyword);
+                    ReaderJson.FoodItem lowestCalorieItem = findItemWithLowestCalories(filteredItems);
+                    if (lowestCalorieItem != null) {
+                        String formattedItem = formatFoodItem(lowestCalorieItem);
+                        lOutput.add(formattedItem);
+                    }
+                }
+                for (String keyword : dinnerInputs) {
+                    List<ReaderJson.FoodItem> filteredItems = searchFoodItems(foodItems, keyword);
+                    ReaderJson.FoodItem lowestCalorieItem = findItemWithLowestCalories(filteredItems);
+                    if (lowestCalorieItem != null) {
+                        String formattedItem = formatFoodItem(lowestCalorieItem);
+                        dOutput.add(formattedItem);
+                    }
+                }
+                for (String keyword : snacksInputs) {
+                    List<ReaderJson.FoodItem> filteredItems = searchFoodItems(foodItems, keyword);
+                    ReaderJson.FoodItem lowestCalorieItem = findItemWithLowestCalories(filteredItems);
+                    if (lowestCalorieItem != null) {
+                        String formattedItem = formatFoodItem(lowestCalorieItem);
+                        sOutput.add(formattedItem);
+                    }
+                }
+
+                calorie = input_calorie.getText().toString();
+
+                if(breakfastInputs.size() == 0 || lunchInputs.size() == 0 || dinnerInputs.size() == 0 || lunchInputs.size() == 0){
+                    Toast.makeText(getApplicationContext(), "All section must have one meal!", Toast.LENGTH_SHORT).show();
+                }
+                else if(calorie.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Calorie input cannot be empty!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    try {
+                        double calorieValue = Double.parseDouble(calorie);
+
+                        if (calorieValue <= 0) {
+                            Toast.makeText(getApplicationContext(), "Calorie input must be a positive number!", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Main m = new Main();
+                            m.getInput(bOutput, lOutput, dOutput, sOutput, calorie);
+                        }
+                    }
+                    catch (NumberFormatException e) {
+                        Toast.makeText(getApplicationContext(), "Invalid calorie input! Please enter a valid number!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
-    // Method to search for FoodItems containing a specific keyword in the name
     private List<ReaderJson.FoodItem> searchFoodItems(List<ReaderJson.FoodItem> foodItems, String keyword) {
         List<ReaderJson.FoodItem> filteredItems = new ArrayList<>();
         for (ReaderJson.FoodItem item : foodItems) {
@@ -154,7 +192,6 @@ public class HomeActivity extends AppCompatActivity {
         return filteredItems;
     }
 
-    // Method to find the item with the lowest calories in a list of FoodItems
     private ReaderJson.FoodItem findItemWithLowestCalories(List<ReaderJson.FoodItem> foodItems) {
         if (foodItems.isEmpty()) {
             return null;
@@ -169,15 +206,11 @@ public class HomeActivity extends AppCompatActivity {
         return lowestCalorieItem;
     }
 
-
-
-
-
-
-
-
-
-
+    private String formatFoodItem(ReaderJson.FoodItem item) {
+        return String.format("%s, %d, %d, %.1f, %d, %d, %d",
+                item.getName(), item.getCalories(), item.getSugar(), item.getSaturatedFat(),
+                item.getSodium(), item.getFiber(), item.getProtein());
+    }
 
     private void addNewRow(final LinearLayout section) {
         final LinearLayout newRow = new LinearLayout(this);
